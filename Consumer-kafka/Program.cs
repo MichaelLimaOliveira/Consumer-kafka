f224b85de94b434f1abdcb72d3ee82161eaeca2d
Michael <Michael@DESKTOP-OCE1G6O>
2023-02-24 10:38:07 -0300

    Adicionar arquivos de projeto.

---
+++ Consumer-kafka/Program.cs
@@ -0,0 +1,38 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:29092",
+    GroupId = "creditengine",
+    AutoOffsetReset = AutoOffsetReset.Earliest,
+    EnableAutoOffsetStore = false
+};
+
+using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+consumer.Subscribe("teste-kafka");
+
+CancellationTokenSource token = new();
+
+try
+{
+    while (true)
+    {
+        var response = consumer.Consume(token.Token);
+        if (response.Message != null)
+        {
+            var weather = JsonConvert.DeserializeObject<Weather>
+                (response.Message.Value);
+            Console.WriteLine($"State: {weather.State}, " +
+                $"Temp: {weather.Temparature}F");
+        }
+    }
+}
+catch (Exception)
+{
+
+    throw;
+}
+
+public record Weather(string State, int Temparature);
